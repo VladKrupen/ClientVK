76908fcda8013843bdbc5d80787940e809fb08e7
Vlad Krupen <air7360@gmail.com>
2024-12-02 12:41:32 +0300

    Initial commit

---
+++ ClientVK/Application/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  ClientVK
+//
+//  Created by Vlad on 2.12.24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return .portrait
+    }
+}
+
---
+++ ClientVK/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ClientVK
+//
+//  Created by Vlad on 2.12.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
